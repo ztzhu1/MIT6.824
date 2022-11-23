510e1354b7a382c915e672034d66307a7f2ed50e
ztzhu1 <zhuzitian@zju.edu.cn>
2022-11-24 00:39:30 +0800

    Map to intermediate file

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -5,7 +5,10 @@ import (
 	"hash/fnv"
 	"log"
 	"net/rpc"
-
+	"os"
+	"io/ioutil"
+	"sort"
+	"errors"
 	"6.824/assert"
 )
 
@@ -16,6 +19,14 @@ type KeyValue struct {
 	Key   string
 	Value string
 }
+
+// for sorting by key.
+type ByKey []KeyValue
+
+// for sorting by key.
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
 
 //
 // use ihash(key) % NReduce to choose the reduce
@@ -45,13 +56,19 @@ func Worker(mapf func(string, string) []KeyValue,
 
 	if task.Type == FAKE {
 		fmt.Printf("\033[1;34m")
-		fmt.Printf("Received fake task. Quit worker.\n")
+		fmt.Printf("Received fake task. Worker quited.\n")
 		fmt.Printf("\033[0m")
-		return
 	} else {
 		fmt.Printf("\033[1;32m")
 		fmt.Printf("id: %v, in: %v, out: %v\n", task.Id, task.InputName, task.OutputName)
 		fmt.Printf("\033[0m")
+
+		contentPtr := read(task.InputName)
+		mapHelper(task.InputName,
+				  task.OutputName + "-",
+				  contentPtr,
+				  mapf)
+		// result := reduceHelper(kva, reducef)
 	}
 }
 
@@ -107,4 +124,75 @@ func call(rpcname string, args interface{}, reply interface{}) bool {
 	fmt.Println(err)
 	fmt.Printf("\033[0m")
 	return false
+}
+
+func read(filename string) *string {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("cannot open %v", filename)
+	}
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", filename)
+	}
+	file.Close()
+
+	ret := string(content)
+	return &ret
+}
+
+func mapHelper(inputName  string,
+			   outputName string,
+			   contentPtr *string,
+			   mapf func(string, string) []KeyValue) {
+	kva := mapf(inputName, *contentPtr)
+	sort.Sort(ByKey(kva))
+
+	ofile, _ := os.CreateTemp("./", outputName)
+	for _, kv := range kva {
+		fmt.Fprintf(ofile, "%v %v\n", kv.Key, kv.Value)
+	}
+	ofile.Close()
+	renameOrDrop(ofile.Name())
+}
+
+func reduceHelper(mapFile string, reducef func(string, []string) string) {
+	intermediate := []KeyValue{}
+	intermediate = append(intermediate, kva...)
+	sort.Sort(ByKey(intermediate))
+
+	result := []KeyValue{}
+
+	i := 0
+	for i < len(intermediate) {
+		j := i + 1
+		for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+			j++
+		}
+		values := []string{}
+		for k := i; k < j; k++ {
+			values = append(values, intermediate[k].Value)
+		}
+		output := reducef(intermediate[i].Key, values)
+
+		// this is the correct format for each line of Reduce output.
+		// fmt.Fprintf(ofile, "%v %v\n", intermediate[i].Key, output)
+		result = append(result, KeyValue{intermediate[i].Key, output})
+
+		i = j
+	}
+}
+
+func renameOrDrop(filename string) bool {
+	var renamed bool
+	file := lockfile.Lockfile(filename)
+	err := file.TryLock()
+	if err == nil {
+		exist = true
+	} else if errors.Is(err, os.ErrNotExist) {
+		exist = false
+	} else {
+		assert.Assert(false)
+	}
+	return renamed
 }
