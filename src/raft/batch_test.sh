d71f3571436de01b906a2e12455fe87631d100d5
ztzhu1 <zhuzitian@zju.edu.cn>
2023-03-25 17:04:54 +0800

    test many times

---
+++ src/raft/batch_test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if [[ -z $1 ]] || [[ -z $2 ]]
+then
+    echo "usage: batch_test 'test name' 'repeat num'"
+    exit
+fi
+
+fail=false
+trap ctrl_c INT
+function ctrl_c() {
+    fail=true
+}
+
+for (( i=0; i < $2 && !fail; ++i ))
+do
+    echo -e "\x1b[1;34m${i}th test begins\x1b[0m"
+    bash ./test.sh $1 | grep -ie "fail[^u]" && fail=true
+done
+
+if [[ $fail = "false" ]]
+then
+    echo -e "\x1b[1;32mPass\x1b[0m"
+else
+    echo -e "\x1b[1;31mFail\x1b[0m"
+fi
