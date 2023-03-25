38666830615d2e371f16dbf32935d84f0ec6e2c0
ztzhu1 <zhuzitian@zju.edu.cn>
2023-03-25 11:23:01 +0800

    utils

--- src/raft/util.go
+++ src/raft/util.go
@@ -3,11 +3,25 @@ package raft
 import "log"
 
 // Debugging
-const Debug = false
+var Debug bool = false
+
+func InitLog(enable bool) {
+	if enable {
+		Debug = true
+		log.SetFlags(log.Ltime | log.Lmicroseconds)
+	}
+}
 
 func DPrintf(format string, a ...interface{}) (n int, err error) {
 	if Debug {
 		log.Printf(format, a...)
+	}
+	return
+}
+
+func DWarning(format string, a ...interface{}) (n int, err error) {
+	if Debug {
+		log.Printf("\x1b[1;33m"+format+"\x1b[0m", a...)
 	}
 	return
 }
