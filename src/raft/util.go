912ff27ffb22736636a40d4f7f5fec2d0bc276f2
ztzhu1 <zhuzitian@zju.edu.cn>
2023-03-25 14:49:38 +0800

    Support different types of log

--- src/raft/util.go
+++ src/raft/util.go
@@ -19,6 +19,13 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
+func DInfo(format string, a ...interface{}) (n int, err error) {
+	if Debug {
+		log.Printf("\x1b[1;34m"+format+"\x1b[0m", a...)
+	}
+	return
+}
+
 func DWarning(format string, a ...interface{}) (n int, err error) {
 	if Debug {
 		log.Printf("\x1b[1;33m"+format+"\x1b[0m", a...)
