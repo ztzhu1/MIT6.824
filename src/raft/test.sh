0282c3d631d5afcea01e2309bc0a97f79aabedd2
ztzhu1 <zhuzitian@zju.edu.cn>
2023-03-25 14:43:12 +0800

    Colorize test output

--- src/raft/test.sh
+++ src/raft/test.sh
@@ -2,6 +2,22 @@
 esc=$(printf "\033")
 if [[ -n $1 ]]
 then
-    flag="-run $1"
+    if [[ $1 == "-race" ]]
+    then
+        flag="$1"
+    else
+        flag="-run $1"
+    fi
 fi
-go test ${flag} -race | sed -u -E "s/(.*?pass.*?)/${esc}[1;32m\1${esc}[0m/gi" | sed -E "s/(.*?fail[^u].*?)/${esc}[1;31m\1${esc}[0m/gi"
+
+if [[ -n $2 ]]
+then
+    if [[ $2 == "-race" ]]
+    then
+        flag="${flag} $2"
+    else
+        flag="${flag} -run $2"
+    fi
+fi
+
+go test ${flag} | sed -u -E "s/(.*?pass.*?)/${esc}[1;32m\1${esc}[0m/gi" | sed -u -E "s/(.*?fail[^u].*?)/${esc}[1;31m\1${esc}[0m/gi" | sed -u -E "s/(.*?Test .*?)/${esc}[1;35m\1${esc}[0m/gi"
