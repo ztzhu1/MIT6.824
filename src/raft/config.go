b37400d0468edd35a57671cff48c5286b1ff4963
ztzhu1 <zhuzitian@zju.edu.cn>
2023-03-25 14:43:26 +0800

    Colorize test output

--- src/raft/config.go
+++ src/raft/config.go
@@ -359,6 +359,7 @@ func (cfg *config) cleanup() {
 // attach server i to the net.
 func (cfg *config) connect(i int) {
 	// fmt.Printf("connect(%d)\n", i)
+	DInfo("%v connected", i)
 
 	cfg.connected[i] = true
 
@@ -382,6 +383,7 @@ func (cfg *config) connect(i int) {
 // detach server i from the net.
 func (cfg *config) disconnect(i int) {
 	// fmt.Printf("disconnect(%d)\n", i)
+	DInfo("%v disconnected", i)
 
 	cfg.connected[i] = false
 
