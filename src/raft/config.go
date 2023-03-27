cc89984c78daf87cc42fb74de748069c65f9d269
ztzhu1 <zhuzitian@zju.edu.cn>
2023-03-28 00:28:44 +0800

    Change log function name

--- src/raft/config.go
+++ src/raft/config.go
@@ -359,7 +359,7 @@ func (cfg *config) cleanup() {
 // attach server i to the net.
 func (cfg *config) connect(i int) {
 	// fmt.Printf("connect(%d)\n", i)
-	DInfo("%v connected", i)
+	DSysInfo("%v connected", i)
 
 	cfg.connected[i] = true
 
@@ -383,7 +383,7 @@ func (cfg *config) connect(i int) {
 // detach server i from the net.
 func (cfg *config) disconnect(i int) {
 	// fmt.Printf("disconnect(%d)\n", i)
-	DInfo("%v disconnected", i)
+	DSysInfo("%v disconnected", i)
 
 	cfg.connected[i] = false
 
